5d6b5f0acca6acbf3a218ce8d1f140dd6e89d369
hsinchi-yen <60861761+hsinchi-yen@users.noreply.github.com>
2021-08-10 15:18:19 +0800

    Add files via upload

--- autoredminemail/mailtest.py
+++ autoredminemail/mailtest.py
@@ -1,11 +1,19 @@
+#This is a python script for sending reminder email to bug review meeting members.
+#Created date : 20210809
+#Applied Platform : Windows 10 , Office 365 Outlook
+#Author : Lance Yen
+
 import smtplib,time
+import re
+import os
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
+from datetime import datetime
 
 # Preconfig the mail from and mail to ids
-def mail_list_predefine():
+def mail_list_predefine(paramfile):
     lineidx = 0
-    mmt = open('MAIL_TR_DEF.txt', 'r',encoding='UTF-8')
+    mmt = open(paramfile, 'r',encoding='UTF-8')
     for line in mmt:
         if lineidx == 1:
             r_username = line
@@ -28,6 +36,33 @@ def mail_list_predefine():
 
     return r_mail_from,r_mail_receipts,r_mail_subject
 
+def body_date_modify():
+    now = datetime.now()
+
+    line_idx = 0
+    cur_date = now.strftime("%m/%d")
+
+    f = open('RM_MSG_BODY.txt', 'r', encoding='UTF-8')
+    fw = open('NEW_RM_MSG_BODY.txt', 'w', encoding='UTF-8')
+
+    for line in f:
+
+        if line_idx == 2:
+            newline = re.sub('\d{1,2}\/\d{1,2}', cur_date, line)
+            fw.writelines(newline)
+        else:
+            fw.writelines(line)
+
+        line_idx += 1
+
+    f.close()
+    fw.close()
+    time.sleep(1)
+
+    os.remove("RM_MSG_BODY.txt")
+    os.rename("NEW_RM_MSG_BODY.txt", "RM_MSG_BODY.txt")
+    time.sleep(1)
+
 
 username = "lance.yen@technexion.com"
 password = "vzynvdfrdmcnkgmb"
@@ -35,12 +70,14 @@ password = "vzynvdfrdmcnkgmb"
 #mail_receipts = "hsinchi.yen@gmail.com,lance.yen@technexion.com"
 #mail_subject = "Please Update Redmine Issues"
 
-mail_from, mail_receipts, mail_subject = mail_list_predefine()
+#load parameters from external file
+mail_from, mail_receipts, mail_subject = mail_list_predefine('MAIL_TR_DEF.txt')
 
-#import a mail body from external text file RM_MSB_BODY.TXT
+#START - import a mail body from external text file RM_MSB_BODY.TXT
 msgbody = open('RM_MSG_BODY.txt','r',encoding='UTF-8')
 mail_body = msgbody.read()
 msgbody.close()
+#END - import a mail body from external text file RM_MSB_BODY.TXT
 
 mimemsg = MIMEMultipart()
 mimemsg['From']=mail_from
